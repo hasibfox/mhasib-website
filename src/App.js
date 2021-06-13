fdce8ca9af4c512713ee90d44bd2c4e954c57c38
hasibfox <hasibfox@gmail.com>
2021-06-13 05:34:25 -0400

    colored-icons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Falling from './components/Falling';
 import Navbar from './components/Navbar';
 import Page from './components/Page';
-import icons from './components/icons.json'
+import icons_color from './components/icons_color.json';
 
 function App() {
   return (
     <div>
       <div className="icons-container">
-        {icons.map((icon)=>{
+        {icons_color.map((icon)=>{
           return (
             <Falling emote={icon.emote} size={icon.size} speed={icon.speed} delay={icon.delay} color={icon.color} reverse={icon.reverse} dist={icon.dist}/>
           )
