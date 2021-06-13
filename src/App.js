467f294aba6944dee2414b9aa72dbe87601faff6
hasibfox <hasibfox@gmail.com>
2021-06-13 05:22:48 -0400

    mobile-responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Falling from './components/Falling';
 import Navbar from './components/Navbar';
 import Page from './components/Page';
@@ -8,7 +7,7 @@ function App() {
   return (
     <div>
       <div className="icons-container">
-        {(icons.sort(() => Math.random() - 0.5)).map((icon)=>{
+        {icons.map((icon)=>{
           return (
             <Falling emote={icon.emote} size={icon.size} speed={icon.speed} delay={icon.delay} color={icon.color} reverse={icon.reverse} dist={icon.dist}/>
           )
