a4cf524fc7c0702239e896bb76222be69935712b
hasibfox <hasibfox@gmail.com>
2021-07-29 00:53:47 -0400

    modal-title

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,7 +8,7 @@ const Modal = ({id, desc, images}) => {
                 <div class="modal-dialog modal-dialog-centered" style="max-width: 80%" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h2 class="modal-title" id="exampleModalLongTitle">Tajweed Website</h2>
+                            <h2 class="modal-title" id="exampleModalLongTitle"></h2>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
