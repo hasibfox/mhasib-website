a9e86707c0a49938161d52c8f4236b062b753150
hasibfox <hasibfox@gmail.com>
2022-01-10 10:41:31 -0500

    animation

--- src/components/Falling.js
+++ src/components/Falling.js
@@ -3,14 +3,11 @@ import './index.css'
 const Falling = ({emote,size,speed,delay,color,reverse,dist}) => {
     return (
         <div>
-            <div className="" style={{
+            <div className="ii" style={{
                 fontSize: size,
                 color:color,
             }}>
                 <i className={emote} style={{
-                    animation:"rotate " + speed + " infinite " ,
-                    animationDelay: delay,
-                    animationDirection: reverse,
                     left: dist,
                 }}></i>
                 </div>
